2621ff5e75d221e5fc931ac2733a0e2672bf49c1
Howard <howardguoui@gmail.com>
2018-10-05 16:35:47 -0400

    Updates

---
+++ precache-manifest.613036f5e326feb01e349b8d198a0ce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/howardreact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "29650c0e768148b57d2d",
+    "url": "/howardreact/static/js/runtime~main.29650c0e.js"
+  },
+  {
+    "revision": "d73c191185e4ed23c6d0",
+    "url": "/howardreact/static/js/main.d73c1911.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/howardreact/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "d73c191185e4ed23c6d0",
+    "url": "/howardreact/static/css/main.9d8d99a9.chunk.css"
+  },
+  {
+    "revision": "dbb7b3cc46501308ec769bd970d0528b",
+    "url": "/howardreact/index.html"
+  }
+];
