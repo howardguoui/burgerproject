eeb86f394e1397562b86f4780d8025eb775c3244
Howard <howardguoui@gmail.com>
2018-10-11 17:21:20 -0400

    adding price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -17,6 +17,7 @@ const controls = [
 ]
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
             <BuildControl 
             key={ctrl.label} 
