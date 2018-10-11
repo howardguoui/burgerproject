06395b51c19e4d2aac776f44c7f0db2a874b0114
Howard <howardguoui@gmail.com>
2018-10-11 15:55:23 -0400

    add addingredient logic into build control

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+const INGREDIENT_PRICES = {
+    salad: 1,
+    cheese: 1.5,
+    meat: 3,
+    bacon: 2
+}
 class BurgerBuilder extends Component {
     // constructor(props) {
     //     super(props);
@@ -14,13 +20,29 @@ class BurgerBuilder extends Component {
             bacon : 1,
             cheese: 2,
             meat: 2
-        }
+        },
+        totalPrice: 10
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCounted = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCounted;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     }
     render() {
         return (
             <Aux>
                 <Burger ingredients= {this.state.ingredients} />
-                <BuildControls />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                />
             </Aux>
         );
     }
