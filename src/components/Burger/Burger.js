0afd24efa5448487f72a2ea86ca1c24d5a31e907
Howard <howardguoui@gmail.com>
2018-10-11 14:46:23 -0400

    adding reduce method

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,15 @@ import React from 'react';
 import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
-    // const transformedIngredients = Object.keys(props.ingredients)
-    //     .map(igKey => {
-    //         console.log();
-    //         return [...Array(props.ingredients[igKey])].map((_, i)=>{
-    //             return <BurgerIngredient key={igKey + i} type={igKey} />;
-    //         });
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            console.log();
+            return [...Array(props.ingredients[igKey])].map((_, i)=>{
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        }).reduce((arr, el) =>{
+            return arr.concat(el)
+        }, []);
     // 1].Object.keys(props.ingredients) 
     // [salad, bacon, meat, cheese].      
     // 2] map(igKey => { return. }) 
@@ -21,31 +24,31 @@ const burger = (props) => {
     // e.g. For salad it will iterate 2 times and return 
     // <BurgerIngredient key=”salad0” type=”salad” /> 
     // <BurgerIngredient key=”salad1” type=”salad” 
-    // });
+
           /*
   Declare an ingredients array that will hold the BurgerIngredient
   components.
   */
-  let ingredients = [];
+//   let ingredients = [];
  
-  // Loop through every key in the props.ingredients object
-  for (const ingKey in props.ingredients) {
-    /*
-    For every key, use a for loop that iterates as many times as the value
-    contained in each key. For every iteration, add a BurgerIngredient to the
-    ingredients array.
-    */
-    for (let i = 0; i < props.ingredients[ingKey]; i++) {
-      ingredients.push(<BurgerIngredient key={ingKey + i} type={ingKey} />);
-    }
-  }
-  if (ingredients.length === 0) {
-    ingredients = <p>Please start adding ingredients!</p>;
+//   // Loop through every key in the props.ingredients object
+//   for (const ingKey in props.ingredients) {
+//     /*
+//     For every key, use a for loop that iterates as many times as the value
+//     contained in each key. For every iteration, add a BurgerIngredient to the
+//     ingredients array.
+//     */
+//     for (let i = 0; i < props.ingredients[ingKey]; i++) {
+//       ingredients.push(<BurgerIngredient key={ingKey + i} type={ingKey} />);
+//     }
+//   }
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
   }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            {ingredients}
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
