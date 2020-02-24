456df866038460cac4f24dd95e6f5aa47232b2dd
Howard <howardguoui@gmail.com>
2020-02-24 00:02:05 -0500

    minor changes

--- src/components/Main/Blog.js
+++ src/components/Main/Blog.js
@@ -12,9 +12,7 @@ import FeaturedPost from './FeaturedPost';
 import Main from './Main';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
-import post1 from './blog-post.1.md';
-import post2 from './blog-post.2.md';
-import post3 from './blog-post.3.md';
+
 
 const useStyles = makeStyles(theme => ({
   mainGrid: {
@@ -25,23 +23,16 @@ const useStyles = makeStyles(theme => ({
 const sections = [
   { title: 'Technology', url: '#' },
   { title: 'Design', url: '#' },
-  { title: 'Culture', url: '#' },
-  { title: 'Business', url: '#' },
-  { title: 'Politics', url: '#' },
-  { title: 'Opinion', url: '#' },
-  { title: 'Science', url: '#' },
-  { title: 'Health', url: '#' },
   { title: 'Style', url: '#' },
   { title: 'Travel', url: '#' },
 ];
 
 const mainFeaturedPost = {
-  title: 'Title of a longer featured blog post',
+  title: 'Welcome to my React home page',
   description:
-    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+    "This page will continue integration of new UI feature relate to react.",
   image: 'https://source.unsplash.com/random',
   imgText: 'main image description',
-  linkText: 'Continue reading…',
 };
 
 const featuredPosts = [
@@ -56,24 +47,15 @@ const featuredPosts = [
   },
 ];
 
-const posts = [post1, post2, post3];
+//TODO: add content
+const posts = [];
 
 const sidebar = {
   title: 'About',
   description:
-    'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
+    'I am UI developer who is mainly focus on React, Angular, Vue, Python and JAVA',
   archives: [
-    { title: 'March 2020', url: '#' },
-    { title: 'February 2020', url: '#' },
-    { title: 'January 2020', url: '#' },
-    { title: 'November 1999', url: '#' },
-    { title: 'October 1999', url: '#' },
-    { title: 'September 1999', url: '#' },
-    { title: 'August 1999', url: '#' },
-    { title: 'July 1999', url: '#' },
-    { title: 'June 1999', url: '#' },
-    { title: 'May 1999', url: '#' },
-    { title: 'April 1999', url: '#' },
+    { title: 'Feb 2020', url: '/' },
   ],
   social: [
     { name: 'GitHub', icon: GitHubIcon },
@@ -89,7 +71,7 @@ export default function Blog() {
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Header title="Blog" sections={sections} />
+        <Header title="Howard React Main Page" sections={sections} />
         <main>
           <MainFeaturedPost post={mainFeaturedPost} />
           <Grid container spacing={4}>
@@ -98,7 +80,7 @@ export default function Blog() {
             ))}
           </Grid>
           <Grid container spacing={5} className={classes.mainGrid}>
-            <Main title="From the firehose" posts={posts} />
+            <Main title="Trend" posts={posts} />
             <Sidebar
               title={sidebar.title}
               description={sidebar.description}
--- src/components/Main/Footer.js
+++ src/components/Main/Footer.js
@@ -9,8 +9,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" href="https://github.com/howardguoui">
+        Howardguoui Github
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
