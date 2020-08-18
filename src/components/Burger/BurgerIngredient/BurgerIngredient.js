58db0e2932020d7b394512bc44951d5a1a16fb5a
Howard <howardguoui@gmail.com>
2020-08-17 23:56:33 -0400

    change to hook

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,48 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import clases from './BurgerIngredient.css'
-class BurgerIngredient extends Component {
-    render() {
-        
-            let ingredient = null;
-            switch (this.props.type) {
-                case('bread-bottom'):
-                    ingredient = <div className={clases.BreadBottom}></div>
-                    break;
-                case('bread-top'):
-                    ingredient = (
-                        <div className={clases.BreadTop}>
-                     <div className={clases.Seeds1}>
-                    
-                    </div>
-                    <div className={clases.Seeds2}>
-                    
-                    </div>
-                    </div>
-                    )
-                    break;
-                case('meat'):
-                    ingredient = <div className={clases.Meat}></div>
-                    break;
-                case('cheese'):
-                    ingredient = <div className={clases.Cheese}></div>
-                    break;
-                case('lettuce'):
-                    ingredient = <div className={clases.Lettuce}></div>
-                    break;
-                case('bacon'):
-                    ingredient = <div className={clases.Bacon}></div>
-                    break;
-                case('tomato'):
-                    ingredient = <div className={clases.Tomato}></div>
-                    break;
-                default: 
-                ingredient = null;
-            }
-            return ingredient;
-        }
-}
-BurgerIngredient.propTypes = {
-    type: PropTypes.string.isRequired
-}
-export default BurgerIngredient;
+
+import classes from './BurgerIngredient.css';
+
+const burgerIngredient = (props) => {
+  let ingredient = null;
+
+  switch (props.type) {
+    case 'bread-bottom':
+      ingredient = <div className={classes.BreadBottom} />;
+      break;
+    case 'bread-top':
+      ingredient = (
+        <div className={classes.BreadTop}>
+          <div className={classes.Seeds1} />
+          <div className={classes.Seeds2} />
+        </div>
+      );
+      break;
+    case 'meat':
+      ingredient = <div className={classes.Meat} />;
+      break;
+    case 'cheese':
+      ingredient = <div className={classes.Cheese} />;
+      break;
+    case 'bacon':
+      ingredient = <div className={classes.Bacon} />;
+      break;
+    case 'salad':
+      ingredient = <div className={classes.Salad} />;
+      break;
+    default:
+      ingredient = null;
+  }
+
+  return ingredient;
+};
+
+burgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired,
+};
+
+export default burgerIngredient;
--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,22 +1,34 @@
 import React from 'react';
+
 import Button from '../../UI/Button/Button';
-const orderSumary = (props) => {
-    const ingredientSumary = Object.keys(props.ingredients).map(igKey => {
-        return <li key={igKey}><span style={{textTransform:'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
-    });
+
+const orderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
     return (
-        <React.Fragment>
-            <h3>Your Order</h3>
-            <p>A burger sumary</p>
-            <ul>
-                {ingredientSumary}
-            </ul>
-            <p><strong>Total Price: </strong>{props.price}</p>
-            <p>Continue to check out? </p>
-            <Button btnType="Danger" clicked= {props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked= {props.purchaseContinued}>CONTINUE</Button>
-        </React.Fragment>
+      <li key={igKey}>
+        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+        {props.ingredients[igKey]}
+      </li>
     );
+  });
+
+  return (
+    <React.Fragment>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
+    </React.Fragment>
+  );
 };
 
-export default orderSumary
+export default orderSummary;
