06e3d00d0f2060f4563fcca16bcbea7c07ad5da8
Howard <howardguoui@gmail.com>
2020-08-18 00:28:37 -0400

    Updates

---
+++ precache-manifest.7858aa9cc9d89015fe5d3b3df858b407.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6956a6e6bbbc33c4d081",
+    "url": "/howardreact/static/css/main.3b5a281d.chunk.css"
+  },
+  {
+    "revision": "6956a6e6bbbc33c4d081",
+    "url": "/howardreact/static/js/main.6956a6e6.chunk.js"
+  },
+  {
+    "revision": "500a36aae0a8ecbcc018",
+    "url": "/howardreact/static/js/runtime~main.500a36aa.js"
+  },
+  {
+    "revision": "24655b1162d8eb9ded96",
+    "url": "/howardreact/static/js/2.24655b11.chunk.js"
+  },
+  {
+    "revision": "727867be08541974c596",
+    "url": "/howardreact/static/css/3.bcbb7588.chunk.css"
+  },
+  {
+    "revision": "727867be08541974c596",
+    "url": "/howardreact/static/js/3.727867be.chunk.js"
+  },
+  {
+    "revision": "a53083445f2da5bb7432",
+    "url": "/howardreact/static/css/4.a30da495.chunk.css"
+  },
+  {
+    "revision": "a53083445f2da5bb7432",
+    "url": "/howardreact/static/js/4.a5308344.chunk.js"
+  },
+  {
+    "revision": "86eadcedf667b683fe3b",
+    "url": "/howardreact/static/css/5.ead2fcab.chunk.css"
+  },
+  {
+    "revision": "86eadcedf667b683fe3b",
+    "url": "/howardreact/static/js/5.86eadced.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/howardreact/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "fd43ff91b94bfad8a156cf86eb123473",
+    "url": "/howardreact/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/howardreact/precache-manifest.1b82a36c9d8054388914c52824ce490a.js"
+  "/howardreact/precache-manifest.7858aa9cc9d89015fe5d3b3df858b407.js"
 );
 
 workbox.clientsClaim();
