4dbe18b2a4db577c967148250c035b730b1629c3
Howard <howardguoui@gmail.com>
2020-09-01 23:12:08 -0400

    Updates

---
+++ precache-manifest.fb4a082b6e7fa78a1b58c0644f5618d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "20d4708b84e33c5ba372",
+    "url": "/burgerproject/static/css/main.60b9812b.chunk.css"
+  },
+  {
+    "revision": "20d4708b84e33c5ba372",
+    "url": "/burgerproject/static/js/main.20d4708b.chunk.js"
+  },
+  {
+    "revision": "ee374debcb15a369d3a5",
+    "url": "/burgerproject/static/js/runtime~main.ee374deb.js"
+  },
+  {
+    "revision": "94fba91c4352aaeaf1fe",
+    "url": "/burgerproject/static/js/2.94fba91c.chunk.js"
+  },
+  {
+    "revision": "da62b489956dec403a0f",
+    "url": "/burgerproject/static/css/3.bd397fc9.chunk.css"
+  },
+  {
+    "revision": "da62b489956dec403a0f",
+    "url": "/burgerproject/static/js/3.da62b489.chunk.js"
+  },
+  {
+    "revision": "89b84b2c5b4b5532f192",
+    "url": "/burgerproject/static/css/4.ca087277.chunk.css"
+  },
+  {
+    "revision": "89b84b2c5b4b5532f192",
+    "url": "/burgerproject/static/js/4.89b84b2c.chunk.js"
+  },
+  {
+    "revision": "7870172fe7c3e015cdcb",
+    "url": "/burgerproject/static/css/5.54c49c33.chunk.css"
+  },
+  {
+    "revision": "7870172fe7c3e015cdcb",
+    "url": "/burgerproject/static/js/5.7870172f.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerproject/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "69acb058ab0cd8397165ad4e1784bd08",
+    "url": "/burgerproject/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/howardreact/precache-manifest.7858aa9cc9d89015fe5d3b3df858b407.js"
+  "/burgerproject/precache-manifest.fb4a082b6e7fa78a1b58c0644f5618d3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/howardreact/index.html", {
+workbox.routing.registerNavigationRoute("/burgerproject/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
